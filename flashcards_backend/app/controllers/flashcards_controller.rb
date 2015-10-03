5f84ba235bd6424543a09773615f6993d4c1bb9a
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2015-10-03 15:21:21 -0700

    Add creation of a card

--- flashcards_backend/app/controllers/flashcards_controller.rb
+++ flashcards_backend/app/controllers/flashcards_controller.rb
@@ -10,4 +10,22 @@ class FlashcardsController < ApplicationController
     @flashcard = Flashcard.find(id)
   end
 
+  def new
+    @flashcard = Flashcard.new
+  end
+
+  def create
+    p "******************"
+    @flashcard_config = params["flashcard"]
+    @new_flashcard = Flashcard.new()
+    @new_flashcard.name = @flashcard_config['name']
+    @new_flashcard.description = @flashcard_config['description']
+    @new_flashcard.question = @flashcard_config['question']
+    @new_flashcard.answer = @flashcard_config['answer']
+    @new_flashcard.tags = @flashcard_config['tags']
+    @new_flashcard.save
+    @flashcard = @new_flashcard
+    render "flashcards/show"
+  end
+
 end
